6aafb35057cada7c34f8ca1f7598bb2d9089a856
Nicole Rego <nicole.rego@live.com>
2015-03-12 17:54:52 -0400

    Carousel config, and functioning

--- app/assets/javascripts/shelves.js
+++ app/assets/javascripts/shelves.js
@@ -1,4 +1,14 @@
 $(document).on('ready page:load', function(){
 	$('.closeModalSubmit').on("click", function(){
 		});
+	$(".owl-carousel").owlCarousel({
+		items : 4,
+		navigation : true, 
+		navigationText : ["<",">"],
+		pagination : false,
+		scrollPerPage : false,
+		slideSpeed : 300,
+
+	});
+	
 });

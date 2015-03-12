1fb9d4f65831f5db184f356bf210b756893ff81d
Nicole Rego <nicole.rego@live.com>
2015-03-12 14:37:01 -0400

    clearing and closing forms after ajax edit and add functions

--- app/assets/javascripts/books.js
+++
@@ -1,7 +0,0 @@
-// $(document).on('ready page:load', function(){
-// 	$('.new_book').hide();
-// 	$('.addBookButton').on('click', function(){
-// 		$('.new_book').toggle();
-// 	});
-
-// });
--- app/assets/javascripts/shelves.js
+++ app/assets/javascripts/shelves.js
@@ -1,7 +1,4 @@
 $(document).on('ready page:load', function(){
 	$('.closeModalSubmit').on("click", function(){
-		$('.reveal-modal-bg .field').children().val("");
-		$('.reveal-modal-bg').foundation('reveal', 'close');
-	})
-
+		});
 });
